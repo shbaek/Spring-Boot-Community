65d6485d7239617657e409e0ae0c518bdb36b07f
shbaek <asmita.bella@gmail.com>
2020-01-07 22:54:55 +0900

    게시글 목록 출력

--- src/main/java/com/web/controller/BoardController.java
+++ src/main/java/com/web/controller/BoardController.java
@@ -2,11 +2,14 @@ package com.web.controller;
 
 import com.web.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/board")
@@ -18,8 +21,13 @@ public class BoardController {
     @GetMapping({"", "/"})
     public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model){
         model.addAttribute("board", boardService.findBoardByIdx(idx));
-
         return "/board/form";
+    }
+
+    @GetMapping("/list")
+    public String list(@PageableDefault Pageable pageable, Model model){
+        model.addAttribute("boardList", boardService.findBoardList(pageable));
+        return "/board/list";
     }
 
 
